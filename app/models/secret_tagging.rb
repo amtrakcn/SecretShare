e627e8b9ca294de3d42bd0be3c1dd36824f9d69f
Isaac and Changmin <imurchie@gmail.com>
2013-10-25 17:55:23 -0700

    cleaned error handling

--- app/models/secret_tagging.rb
+++ app/models/secret_tagging.rb
@@ -1,6 +1,8 @@
 class SecretTagging < ActiveRecord::Base
   attr_accessible :tag_id, :secret_id
 
+  validates_uniqueness_of :tag_id, :scope => :secret_id, message: "duplicate tags"
+
   belongs_to :tag
   belongs_to :secret
 end
